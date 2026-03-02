08b3f246bccf3aafcece2d90a821e9f6a41771ff
Michael Houghton <michael.houghton@oireachtas.ie>
2026-03-02 09:47:48 +0000

    Fix for forTemplate()

--- src/models/Link.php
+++ src/models/Link.php
@@ -797,7 +797,7 @@ class Link extends DataObject
      * Renders an HTML anchor attribute for this link
      * @return \SilverStripe\ORM\FieldType\DBHTMLText
      */
-    public function forTemplate()
+    public function forTemplate(): string
     {
         $link = '';
         if ($this->LinkURL) {
